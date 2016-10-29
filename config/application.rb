5180b57bd89580e5ab4d08cb1565fee7ecf54ae2
kieaiaarh <kieaiaarh@gmail.com>
2016-10-29 20:53:39 +0900

    Add default settings

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,19 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 require 'elasticsearch/rails/instrumentation'
+
 module TwitterSearch
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.schema_format = :sql
+    config.active_record.default_timezone = :local
+    config.action_view.field_error_proc = Proc.new { |html_tag, _instance| %Q(#{html_tag}).html_safe }
+    config.autoload_paths += %W(#{config.root}/app/batch/)
+    config.action_dispatch.rescue_responses.update('HTTP::Errors::NotFound' => :not_found)
   end
 end
